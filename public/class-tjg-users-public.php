981b15b1ffc64aafa5e9ef0de18f890f955a1fa2
kalamalahala <solo.driver.bob@gmail.com>
2022-06-01 21:57:59 -0400

    try renaming

--- public/class-tjg-users-public.php
+++ public/class-tjg-users-public.php
@@ -74,7 +74,7 @@ class Tjg_Users_Public {
 		 */
 
 		// Bootstrap v4.6.1
-		wp_enqueue_style( 'Bootstrap v4.6.1', plugin_dir_url( __FILE__ ) . 'css/bootstrap.min.css', array(), '4.6.1', 'all' );
+		wp_enqueue_style( 'bootstrap', plugin_dir_url( __FILE__ ) . 'css/bootstrap.min.css', array(), '4.6.1', 'all' );
 
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/tjg-users-public.css', array(), $this->version, 'all' );
 
@@ -101,7 +101,7 @@ class Tjg_Users_Public {
 		 */
 
 		// Bootstrap v4.6.1 Bundle js
-		wp_enqueue_script( 'Bootstrap v4.6.1 Bundle', plugin_dir_url( __FILE__ ) . 'js/bootstrap.bundle.min.js', array( 'jquery' ), '4.6.1', true );
+		wp_enqueue_script( 'bootstrap', plugin_dir_url( __FILE__ ) . 'js/bootstrap.bundle.min.js', array( 'jquery' ), '4.6.1', true );
 
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/tjg-users-public.js', array( 'jquery' ), $this->version, false );
 
