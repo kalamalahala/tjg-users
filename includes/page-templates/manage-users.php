e43bf93f0e7df580ed81a8ec5f59f5fd5ec5ec3a
kalamalahala <solo.driver.bob@gmail.com>
2022-06-02 18:23:56 -0400

    try again

--- includes/page-templates/manage-users.php
+++ includes/page-templates/manage-users.php
@@ -20,7 +20,8 @@ get_header();
 	
 	$agent_list = Tjg_Users::get_tjg_agents( $agent_number_search );
 
-	print count( $agent_list ) . ' agents found';
+	// recursively count the $agent_list array
+	$agent_count = count( $agent_list, COUNT_RECURSIVE );
 
 	?>
 	</pre>
