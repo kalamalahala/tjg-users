8176e7694f0ca8bcd5afc6020000a67a1bca5714
kalamalahala <solo.driver.bob@gmail.com>
2022-06-01 22:26:40 -0400

    table and css

--- includes/page-templates/manage-users.php
+++ includes/page-templates/manage-users.php
@@ -12,36 +12,36 @@ get_header();
 	<h1>TJG Users</h1>
 	<p>Manage and update TJG Agents</p>
 </header>
+<div class="table-container">
+	<table class="user-table">
+		<thead class="user-table-header">
+			<tr>
+				<th>Name</th>
+				<th>Email</th>
+				<th>Phone</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr class="user-table-row">
+				<?php
+				$output = new Tjg_Users;
+				echo $output->get_tjg_agents;
+				?>
+				<td>John Doe</td>
+				<td>email@email.com</td>
+				<td>123-456-7890</td>
+				<td>
+					<a href="#" class="button">View</a>
+					<a href="#" class="button">Edit</a>
+					<a href="#" class="button">Delete Agent</a>
+					<a href="#" class="confirm-delete button" hidden>Are you sure?</a>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
 
-<section class="user-table">
-	<div class="table-container">
-		<table class="user-table-header">
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Email</th>
-					<th>Phone</th>
-					<th>Actions</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<?php
-					echo 'return stuff here';
-					?>
-					<td>John Doe</td>
-					<td>email@email.com</td>
-					<td>123-456-7890</td>
-					<td>
-						<a href="#" class="button">Edit</a>
-						<a href="#" class="button">Delete Agent</a>
-						<a href="#" class="confirm-delete button" hidden>Are you sure?</a>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-</section>
 
 <?php
 // WP Footer
