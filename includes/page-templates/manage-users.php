e87cd59e36263e7a770bda5e815fb02e07d477bf
kalamalahala <solo.driver.bob@gmail.com>
2022-06-01 20:08:09 -0400

    removed html from template

--- includes/page-templates/manage-users.php
+++ includes/page-templates/manage-users.php
@@ -1,32 +1,19 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
+<?php
+/*
+* Plugin Name:       TJG Users
+* Template page:     manage-users.php
+* Intended slug:    manage-users
+*/
 
-<head> -->
-	<?php
-	/*
-    * Plugin Name:       TJG Users
-    * Template page:     manage-users.php
-    * Intended slug:    manage-users
-    */
+get_header();
 
-	get_header();
+?>
 
-	?>
-	<!-- <meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Manage The Johnson Group Users</title>
-</head> -->
-
-<!-- <body> -->
-
-	<main><h4>Hell yeah new template</h4></main>
-
-<!-- </body> -->
+<main>
+	<h4>Hell yeah new template</h4>
+</main>
 <?php
 // WP Footer
 get_footer();
 // End of Template
 ?>
-
-<!-- </html> -->
