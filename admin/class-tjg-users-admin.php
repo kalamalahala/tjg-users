51bedc420f8c351c1d89b5edc32306cc9b147a32
kalamalahala <solo.driver.bob@gmail.com>
2022-06-08 15:50:05 -0400

    unique identifier, current version number

--- admin/class-tjg-users-admin.php
+++ admin/class-tjg-users-admin.php
@@ -73,7 +73,7 @@ class Tjg_Users_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/tjg-users-admin.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name . 'admin_styling', plugin_dir_url( __FILE__ ) . 'css/tjg-users-admin.css', array(), $this->version, 'all' );
 
 	}
 
